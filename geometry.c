a4c26a052c93f59dfd9de05e9bac75a700169930
Sasha M <5541alex5541@gmail.com>
2022-02-15 10:56:10 +0700

    Add file open function

--- geometry.c
+++ geometry.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main()
+int inOutPut()                          //функция ввода и вывода данных
 {
+    FILE *fp;
+    char name[] = "input.txt";
+    if ((fp = fopen(name, "r")) == NULL) //проверка на существование файла
+    {
+        printf("Не удалось открыть файл!\n");
+        //getchar();
+        return 0;
+    }
+    while(fgets != '(')            //требуемые действия над данными
+
+    fclose(fp);
+    //getchar();
+    return 0;
+}
+
+int main()
+{   
+    inOutPut();
 
     return 0;
 }
