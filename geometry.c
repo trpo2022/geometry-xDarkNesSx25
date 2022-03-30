94512137e9d069e6f3a6f8500876a5b002d06cc7
Sasha M <5541alex5541@gmail.com>
2022-03-30 15:11:53 +0700

    Remake date input way(from file to console)

--- geometry.c
+++ geometry.c
@@ -1,32 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#define SIZE 100
-
-//checking the validity of the input file
-int fileCheck()
-{
-    FILE* file = fopen("input.txt", "r");
-    //checking for the existence of a file
-    if(file == NULL)
-    {
-        printf("Input file does not exist!\n");
-        return 0;
-    }
-
-    //counting rows for line-by-line function
-    int lines = 0;
-    while(!feof(file))
-    {
-        if(fgetc(file) == '\n')
-        {
-            lines++;
-        }
-    }
-
-    fclose(file);
-    return lines;
-}
+#define SIZE 50
 
 //checking the entered data
 int isCorrect()
@@ -35,21 +10,21 @@ int isCorrect()
     char circleStr[] = "circle";
     char triangleStr[] = "triangle";
 
-    //opening and reading a file
-    FILE* file = fopen("input.txt", "r");
     int circleRadius;
-    double circleX = 0, circleY = 0;
+    float circleX = 0, circleY = 0;
     char *figure;
-    char str[SIZE];
-    fgets(str, SIZE, file);
 
-    //separator
-    figure = strtok(str, "(");
+    fgets(figure, 7, stdin);
+
     //if circle, than
     if(strcmp(figure, circleStr) == 0)
     {   
-        printf("Figure - ");
+        printf("Figure : ");
         puts(figure);
+        scanf("(%f %f, %d)", &circleX, &circleY, &circleRadius);
+        printf("X coord: %.1f\n", circleX);
+        printf("Y coord: %.1f\n", circleY);
+        printf("Radius: %d\n", circleRadius);
     }
     //if triangle, than
     else if(strcmp(figure, triangleStr) == 0)
@@ -68,18 +43,6 @@ int isCorrect()
 
 int main()
 {
-    // char str[SIZE];
-    // int lines = fileCheck();
-
-    // FILE* file = fopen("input.txt", "r");
-    // for(int line = 0; line < lines; line++)     //i'll need this later
-    // {                                           //there should be a line-by-line function here
-    //     fgets(str, SIZE, file);
-    //     str[strlen(str) - 1] = '\0';
-        
-    // }
-    // fclose(file);
-    
     isCorrect();
 
     return 0;
